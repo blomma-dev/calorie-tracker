097ba9fc67f3fa6a84ee90f974a7be256652eda5
Joel B <64746667+blomma-dev@users.noreply.github.com>
2023-08-18 16:19:02 +0300

    added structure for upcoming js features

---
+++ src/scripts/main.js
@@ -0,0 +1,30 @@
+// Wait for DOM to load
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    // Put the button into a variable
+    var e = document.getElementById("myForm");
+    
+    // Wait for user to click the button
+    e.addEventListener( "change", function() {
+    
+      // Put the selected value into a variable
+      var myColor = this.color.value;
+      
+      // The "Switch" statement.
+      switch ( myColor ) {
+      
+      case "Blue":
+        alert("Just like the sky!");
+        break;
+  
+      case "Red":
+        alert("Quite daring!");
+        break;
+  
+      case "Green":
+        alert("Like... grass?");
+        break;
+        
+      }
+    }, false);
+  });
---
+++ todo.md
@@ -0,0 +1,5 @@
+TODO
+
+- add plus and minus button for already entered items
+- possible option for viewing actual history data
+- add drop shadow for containers
